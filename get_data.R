84eda6950183020389e6b439ae0406a4fc3bb7bc
Baruque Rodrigues <81967584+BaruqueRodrigues@users.noreply.github.com>
2023-07-04 15:01:56 -0300

    att_script

--- get_data.R
+++ get_data.R
@@ -1,13 +1,53 @@
 library(tidyverse)
 
-# get data ----------------------------------------------------------------
+# Get data ----------------------------------------------------------------
 
-discursos <- speechbr::speech_data(keyword = "",
-                      start_date = "2023-01-01",
-                      end_date = "2023-12-31")
 
+# pegando as informações dos dep na api -----------------------------------
+
+# baixando as info dos deputados na api
+deputados <- readr::read_csv2("https://dadosabertos.camara.leg.br/arquivos/deputados/csv/deputados.csv")
+
+
+# pegando os discursos na api
+
+future::multiprocess(8)
+
+teste <- furrr::future_map(paste0(deputados %>%
+                             filter(idLegislaturaFinal == 57) %>%
+                             pull(uri),
+                           "/discursos?idLegislatura=57"), ~
+      .x %>% 
+      jsonlite::fromJSON())
+
+# Enriquecendo os dados dos discursos com as info dos candidatos na api
+
+discursos <- teste %>% 
+  tibble(data =.) %>% 
+  unnest_wider(data) %>% 
+  unnest_longer(dados) %>%
+  unnest(links) %>% 
+  select(-rel) %>% 
+  mutate(href = str_remove_all(href, "https://dadosabertos.camara.leg.br/api/v2/deputados/") %>% 
+           str_extract("\\d{6}")) %>% 
+  rename(cod_dep = href) %>% 
+  unnest() %>% 
+  janitor::clean_names() %>% 
+  left_join(
+    deputados %>%
+      filter(idLegislaturaFinal == 57) %>% 
+      mutate(uri = str_remove_all(uri, "https://dadosabertos.camara.leg.br/api/v2/deputados/") %>% 
+               str_extract("\\d{6}")) %>% 
+      select(cod_dep = uri, nome, nomeCivil, siglaSexo, cpf),
+    multiple = "first"
+    
+  )
+
+# Escrevendo os dados dos discursos em .rds
 write_rds(discursos, "data/discursos.rds")
 
+
+# Pegando as informações dos candidatos no TSE
 dep_2022 <- electionsBR::candidate_fed(2022) %>% 
   filter(DS_CARGO == "DEPUTADO FEDERAL",
          DS_SIT_TOT_TURNO %in% c("ELEITO",
@@ -20,11 +60,19 @@ dep_2022 <- electionsBR::candidate_fed(2022) %>%
     sg_partido,sg_federacao,dt_nascimento,
     ds_grau_instrucao,ds_estado_civil, ds_ocupacao,
     st_reeleicao)
-  
 
- 
-dados <- left_join(discursos, dep_2022,
-            by = c("orador" = "nm_candidato"))
+# Enriquecendo os dados
+dataset_final <- discursos %>% 
+  left_join(dep_2022,
+            by = c("nomeCivil" = "nm_candidato")) 
 
-dados %>% 
+dataset_final %>% 
   write_rds("data/dataset_final.rds")
+
+
+
+
+
+
+
+
